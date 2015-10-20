2b32e7d0d0f48d2fd081752922354bbc060212b6
andreassbayu <andreass.bayu@gmail.com>
2015-10-20 13:58:54 +0000

    frontend:laporan ins

---
+++ app/model/mpengaduan.php
@@ -0,0 +1,38 @@
+<?php
+class mpengaduan extends Database {
+	
+	var $prefix = "";
+	var $salt = "";
+	function __construct()
+	{
+		// $this->db = new Database;
+		$this->salt = "ovancop1234";
+		$this->token = str_shuffle('cmsaj23y4ywdni237yeisa');
+		$this->date = date('Y-m-d H:i:s');
+
+	}
+
+    function insert_laporan($data)
+    {   
+
+        $res = $this->insert($data,'bsn_pengaduan');
+        if ($res) return $res;
+        return false;
+    }
+
+    function getLatestId()
+    {
+        $sql = "SELECT IDENT_CURRENT('bsn_pengaduan') AS id";
+        $res = $this->fetch($sql,0);
+
+        return $res;
+    }
+
+    function insert_file($data)
+    {
+        $res = $this->insert($data,'bsn_file');
+        if ($res) return $res;
+        return false;
+    }
+}
+?>
